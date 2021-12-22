a9613b122591bf0ab9029820fb20000330cbe0e8
Darion Richards <darionrichards@outlook.com>
2021-12-22 11:49:11 +0000

    Initialised express application
    - created connection.js to configure database connection
    - created scripts within package.json

---
+++ src/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const PORT = process.env.PORT || 4000;
+
+const connection = require("./config/connection");
+
+const routes = require("./routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+const init = async() => {
+    try {
+        await connection.sync({ force: false });
+
+        app.listen(PORT, () =>
+            console.log(`🚀🚀 Server running on http://localhost:${PORT}`)
+        );
+    } catch (err) {
+        console.log(`[ERROR]: Connection to DB failes - ${err.message}`);
+    }
+};
+
+init();
