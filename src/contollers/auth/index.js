af270d31c1ce6aaa8c4e364d861356ee69d16a0d
Darion Richards <darionrichards@outlook.com>
2022-01-05 16:57:10 +0000

    Created controller function for /auth/signup route
    - imported helper functions for payload
    - imported hooks to hash passwords before creating a user in database
    - installed bcrypt package to hash passwords
    - successfully creates a user with hased password in database

--- src/contollers/auth/index.js
+++ src/contollers/auth/index.js
@@ -1,8 +1,37 @@
+const { getPayloadWithValidFieldsOnly } = require("../../helpers/index");
+
+const { User } = require("../../models");
+
 const login = (req, res) => {
     res.send("login");
 };
 const signup = (req, res) => {
-    res.send("signup");
+    try {
+        const payload = getPayloadWithValidFieldsOnly(
+            ["firstName", "lastName", "email", "password"],
+            req.body
+        );
+
+        if (Object.keys(payload).length != 4) {
+            return res.status(400).json({
+                success: false,
+                error: "Please provide valid fields in post body.",
+            });
+        }
+
+        const user = User.create(payload);
+
+        return res.status(200).json({
+            success: true,
+            data: user,
+        });
+    } catch (err) {
+        console.log("[ERROR]: Create user failed");
+        return res.status(500).json({
+            success: false,
+            error: err.message,
+        });
+    }
 };
 const logout = (req, res) => {
     res.send("logout");
