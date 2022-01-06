0e26dc8816d6309654a79894c1c616358f65734c
Darion Richards <darionrichards@outlook.com>
2022-01-06 15:10:32 +0000

    Handle LOGIN form submit
    - included jQuery script within main.handlebars
    - added id's to target HTML elements within login form
    - created function to handleLoginSubmit()

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,22 @@
-console.log("Hello, world!");
+const loginFormElement = $("#login-form");
+
+const handleLoginSubmit = async(event) => {
+    event.preventDefault();
+
+    const email = $("#email-input").val();
+    const password = $("#password-input").val();
+
+    const response = await fetch("/auth/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+};
+
+loginFormElement.on("submit", handleLoginSubmit);
