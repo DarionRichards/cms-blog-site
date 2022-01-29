ff06c8479808936d93608a4a5b20e23ca7cbed4a
Darion Richards <darionrichards@outlook.com>
2022-01-29 21:55:10 +0000

    Heroku

--- src/seeds/index.js
+++ src/seeds/index.js
@@ -8,20 +8,24 @@ const seedComments = require("./seedComments");
 const sequelize = require("../config/connection");
 
 const seedAll = async() => {
-    await sequelize.sync({ force: true });
-    console.log("\n---- DATABASE SYNCED ----\n");
+    try {
+        await sequelize.sync({ force: true });
+        console.log("\n---- DATABASE SYNCED ----\n");
 
-    await seedUsers();
-    console.log("\n---- USERS SEEDED ----\n");
+        await seedUsers();
+        console.log("\n---- USERS SEEDED ----\n");
 
-    await seedBlogs();
-    console.log("\n---- BLOGS SEEDED ----\n");
+        await seedBlogs();
+        console.log("\n---- BLOGS SEEDED ----\n");
 
-    await seedComments();
-    console.log("\n---- COMMENTS SEEDED ----\n");
+        await seedComments();
+        console.log("\n---- COMMENTS SEEDED ----\n");
 
-    console.log("\n---- FINISHED SEEDING ----\n");
-    process.exit(0);
+        console.log("\n---- FINISHED SEEDING ----\n");
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 seedAll();
