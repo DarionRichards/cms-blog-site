84528101d8b8c9a696c31043c304c22987a6795e
Darion Richards <darionrichards@outlook.com>
2022-01-29 21:11:46 +0000

    Connection sequelize

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-const dbHost = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
 
 const dbOptions = {
-    host: dbHost,
+    host: DB_HOST,
     dialect: "mysql",
     port: 3306,
     logging: false,
@@ -15,6 +15,6 @@ const dbOptions = {
 
 const sequelize = process.env.JAWSDB_URL ?
     new Sequelize(process.env.JAWSDB_URL) :
-    new Sequelize(dbName, dbUser, dbPassword, dbOptions);
+    new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, dbOptions);
 
 module.exports = sequelize;
